db1c61d5d465fd80ad339eda6df067b5d41e85da
hdaylin <hdaylin@yahoo.com>
2016-11-16 12:21:20 -0500

    Create recursive.java

---
+++ recursive.java
@@ -0,0 +1,38 @@
+package fibonaccisequence;
+
+/**
+ *
+ * @author daylinhenry
+ */
+public class FibonacciSequence {
+
+    
+private static int index = 0;  
+private static int stop = 20;  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args)  
+{  
+  int n1 = 0;  
+  int n2 = 1;  
+  System.out.println("index: " + index + " --> " +n1);  
+  fibonacciSequence(n1, n2);  
+}  
+  
+public static void fibonacciSequence(int n1, int n2)  
+{  
+  System.out.println("index: " + index + " --> " + n2);  
+    
+  // make sure we have set an ending point so this Java recursion  
+  // doesn't go on forever.  
+  if (index == stop)  
+    return;  
+    
+  // make sure we increment our index so we make progress  
+  // toward the end.  
+  index++;  
+    
+  fibonacciSequence(n2, n1+n2);  
+}  
+}
